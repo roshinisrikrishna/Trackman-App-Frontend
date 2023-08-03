37fc401e44746d29e193115e6755e4e61b812968
roshinisrikrishna <38094811+roshinisrikrishna@users.noreply.github.com>
2023-08-03 14:20:45 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,63 +1,50 @@
-/*!
-
-=========================================================
-* Argon Dashboard React - v1.2.3
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import Index from "views/Index.js";
 import Profile from "views/examples/Profile.js";
-import Maps from "views/examples/Maps.js";
-import Register from "views/examples/Register.js";
-import Login from "views/examples/Login.js";
 import Tables from "views/examples/Tables.js";
+import Login from "views/examples/Login";
+import Register from "views/examples/Register";
 import Icons from "views/examples/Icons.js";
+import * as RiIcons from 'react-icons/ri';
 
 var routes = [
   {
-    path: "/index",
-    name: "Home",
-    icon: "ni ni-tv-2 ",
-    component: <Index />,
-    layout: "/admin",
+      name: "Dashboard",
+      icon: "ni ni-tv-2 ",   
+    childrens: [
+      {
+        path: "/admin/home", // Root path for the parent layout
+        name: "Home",
+        icon: "ni ni-tv-2 ",
+        component: <Index />,
+      },
+      {
+        path: "/admin/user-profile", // Absolute path for the child route starting with "/"
+        name: "User Profile",
+        icon: "ni ni-single-02 ",
+        component: <Profile />,
+      },
+      {
+        path: "/admin/tables", // Absolute path for the child route starting with "/"
+        name: "Travel Logs",
+        icon: "ni ni-bullet-list-67 ",
+        component: <Tables />,
+      }
+    ]
   },
   {
-    path: "/user-profile",
-    name: "User Profile",
-    icon: "ni ni-single-02 ",
-    component: <Profile />,
-    layout: "/admin",
-  },
-  {
-    path: "/tables",
-    name: "Travel Logs",
-    icon: "ni ni-bullet-list-67 ",
-    component: <Tables />,
-    layout: "/admin",
-  },
-  {
-    path: "/login",
+    path: "/login", // Root path for the parent layout
     name: "Login",
     icon: "ni ni-key-25 ",
     component: <Login />,
     layout: "/auth",
   },
   {
-    path: "/register",
+    path: "/register", // Root path for the parent layout
     name: "Register",
     icon: "ni ni-circle-08 ",
     component: <Register />,
     layout: "/auth",
   },
 ];
+
 export default routes;
